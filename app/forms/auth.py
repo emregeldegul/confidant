afd070a6d036d620547b5211b61249a7250b0c39
Yunus Emre Geldegül <yunusemregeldegul@gmail.com>
2022-03-26 14:18:12 +0300

    Update auth.py

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.fields.html5 import DateField
+from wtforms.fields import DateField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
 from app.models.user import User
